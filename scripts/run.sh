3d9f42ad3a173997e33338674bfb3df5260025af
Priyam R <108199880+prygw@users.noreply.github.com>
2025-11-25 01:06:40 -0800

    Update paths for XDP object and binaries

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #run ip a and check interface
 INTERFACE="enp2s0"
-XDP_OBJ="bin/xdp_checker.o"
+XDP_OBJ="../bin/xdp_checker.o"
 XDP_SEC="xdp"
-LOADER="bin/load"
-LISTER="bin/list"
+LOADER="../bin/load"
+LISTER="../bin/list"
 
 if [ $# -eq 0 ]; then
     echo "Usage: $0 {enable|disable|allow|list|clear}"
